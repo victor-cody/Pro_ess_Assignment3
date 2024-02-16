6b6634387f3e9ee9f4d439ece2249ef08411b4dc
Bernard Roche <bjroche@gmail.com>
2024-02-16 17:27:37 +0000

    Question 2,3,4

--- Assign3Q1.java
+++ Assign3Q1.java
@@ -1,5 +1,8 @@
 public class Assign3Q1 {
     public static void main(String args[]) {
+
+// Vowels: a,e,i,o,u 
+
       char someChar='a';
 
       System.out.println("Char is: " + someChar);
---
+++ Assign3Q4.java
@@ -0,0 +1,7 @@
+public class Assign3Q4 {
+    public static void main(String args[]) {
+
+      //Insert your code here!
+
+    }
+}
