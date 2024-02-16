831517dc32d82949437bd577edf2d09ea58c17af
Bernard Roche <bjroche@gmail.com>
2024-02-16 17:35:28 +0000

    Questions done

---
+++ Assign3Q5.java
@@ -0,0 +1,7 @@
+public class Assign3Q5 {
+    public static void main(String args[]) {
+
+      //Insert your code here!
+
+    }
+}
---
+++ Assign3Q6.java
@@ -0,0 +1,7 @@
+public class Assign3Q6 {
+    public static void main(String args[]) {
+
+      //Insert your code here!
+
+    }
+}
