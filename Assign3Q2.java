d943f1b9b4afb69bfd483abb05c36b30735a5985
Bernard Roche <bjroche@gmail.com>
2024-02-16 15:52:27 +0000

    Question 2,3

---
+++ Assign3Q2.java
@@ -0,0 +1,10 @@
+public class Assign3Q2 {
+    public static void main(String args[]) {
+      String currentMonth ="January";
+
+      System.out.println("The current month is: " + currentMonth);
+      
+      //Inset your code here!
+      
+    }
+}
---
+++ Assign3Q3.java
@@ -0,0 +1,10 @@
+public class Assign3Q3 {
+    public static void main(String args[]) {
+      int currentValue = 5;
+
+      System.out.println("The number is: " + currentValue);
+
+      //Inset your code here!
+
+    }
+}
