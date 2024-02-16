b13893ab85d8fd0381657cc6f04358044fe539d7
Bernard Roche <bjroche@gmail.com>
2024-02-16 15:25:48 +0000

    Question 1

---
+++ Assign3Q1.java
@@ -0,0 +1,10 @@
+public class Assign3Q1 {
+    public static void main(String args[]) {
+      char someChar='a';
+
+      System.out.println("Char is: " + someChar);
+      
+      //Inset your code here!
+      
+    }
+}
