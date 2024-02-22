160d9459e9525c4974f85533e5b4323fa3e4d145
Victor Ikechukwu <vickyikechukwu13@gmail.com>
2024-02-22 21:44:49 +0000

    solve question 4

--- Assign3Q4.java
+++ Assign3Q4.java
@@ -1,7 +1,18 @@
 public class Assign3Q4 {
-    public static void main(String args[]) {
+  public static void main(String args[]) {
 
-      //Insert your code here!
+    // Insert your code here!
+    int rows = 6;
 
+    for (int index = 0; index < rows; index++) {
+      for (int whitespace = rows - index - 1; whitespace >= 1; whitespace--) {
+        System.out.print(" ");
+      }
+      for (int symbolToPrint = 0; symbolToPrint <= index; symbolToPrint++) {
+        System.out.print("* ");
+      }
+
+      System.out.println();
     }
+  }
 }
