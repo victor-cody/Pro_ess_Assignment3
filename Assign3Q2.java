ac45db29cece9683d3b587aac1b0b973c3e92059
Victor Ikechukwu <vickyikechukwu13@gmail.com>
2024-02-22 22:04:01 +0000

    submission

--- Assign3Q2.java
+++ Assign3Q2.java
@@ -1,7 +1,7 @@
 public class Assign3Q2 {
   public static void main(String args[]) {
     boolean isLeapYear = true;
-    String currentMonth = "January";
+    String currentMonth = "June";
 
     System.out.println("The current month is: " + currentMonth);
 
@@ -9,47 +9,48 @@ public class Assign3Q2 {
 
     switch (currentMonth) {
       case "January":
-        System.out.println(currentMonth + " has 31 day");
+        System.out.println(currentMonth + " has 31 days");
         break;
       case "February":
         if (isLeapYear) {
-          System.out.println(currentMonth + " has 29 day");
+          System.out.println(currentMonth + " has 29 days");
         } else {
-          System.out.println(currentMonth + " has 28 day");
+          System.out.println(currentMonth + " has 28 days");
         }
         break;
       case "March":
-        System.out.println(currentMonth + " has 31 day");
+        System.out.println(currentMonth + " has 31 days");
         break;
       case "April":
-        System.out.println(currentMonth + " has 30 day");
+        System.out.println(currentMonth + " has 30 days");
         break;
       case "May":
-        System.out.println(currentMonth + " has 31 day");
+        System.out.println(currentMonth + " has 31 days");
         break;
       case "June":
-        System.out.println(currentMonth + " has 30 day");
+        System.out.println(currentMonth + " has 30 days");
         break;
       case "July":
-        System.out.println(currentMonth + " has 31 day");
+        System.out.println(currentMonth + " has 31 days");
         break;
       case "August":
-        System.out.println(currentMonth + " has 1 day");
+        System.out.println(currentMonth + " has 1 days");
         break;
       case "September":
-        System.out.println(currentMonth + " has 30 day");
+        System.out.println(currentMonth + " has 30 days");
         break;
       case "October":
-        System.out.println(currentMonth + " has 31 day");
+        System.out.println(currentMonth + " has 31 days");
         break;
       case "November":
-        System.out.println(currentMonth + " has 30 day");
+        System.out.println(currentMonth + " has 30 days");
         break;
       case "December":
-        System.out.println(currentMonth + " has 31 day");
+        System.out.println(currentMonth + " has 31 days");
         break;
 
       default:
+        System.out.println("Invalid, this is not a month in the calender");
         break;
     }
 
--- Assign3Q5.java
+++ Assign3Q5.java
@@ -1,7 +1,25 @@
 public class Assign3Q5 {
-    public static void main(String args[]) {
+  public static void main(String args[]) {
 
-      //Insert your code here!
+    // Insert your code here!
+    int rows = 6;
 
+    for (int index = 1; index <= rows; index++) {
+      for (int whitespace = rows - index - 1; whitespace >= 1; whitespace--) {
+        System.out.print(" ");
+      }
+      for (int symbolToPrint = 0; symbolToPrint <= index; symbolToPrint++) {
+        System.out.print(symbolToPrint + " ");
+      }
+
+      System.out.println();
     }
+
+    // for (i = 1; i <= rows; i++) {
+    // for (j = 1; j <= i; j++) {
+    // System.out.print(i + " ");
+    // }
+    // System.out.println();
+    // }
+  }
 }
