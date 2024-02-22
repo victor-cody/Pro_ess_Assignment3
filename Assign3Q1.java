bbef0cd41f3e78e95ddd034bf079e5d10ba5487a
Victor Ikechukwu <vickyikechukwu13@gmail.com>
2024-02-22 13:38:29 +0000

    Solve question 1

--- Assign3Q1.java
+++ Assign3Q1.java
@@ -1,13 +1,33 @@
 public class Assign3Q1 {
     public static void main(String args[]) {
 
-// Vowels: a,e,i,o,u 
+        // Vowels: a,e,i,o,u
 
-      char someChar='a';
+        char someChar = 'a';
 
-      System.out.println("Char is: " + someChar);
-      
-      //Insert your code here!
-      
+        System.out.println("Char is: " + someChar);
+
+        switch (someChar) {
+            case 'a':
+                System.out.println("The alphabet " + someChar + " is a vowel");
+                break;
+            case 'e':
+                System.out.println("The alphabet " + someChar + " is a vowel");
+                break;
+            case 'i':
+                System.out.println("The alphabet " + someChar + " is a vowel");
+                break;
+            case 'o':
+                System.out.println("The alphabet " + someChar + " is a vowel");
+                break;
+            case 'u':
+                System.out.println("The alphabet " + someChar + " is a vowel");
+                break;
+
+            default:
+                System.out.println("The alphabet " + someChar + " is not a vowel");
+                break;
+        }
+
     }
 }
